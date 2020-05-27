e8f9633b785e9a832d6620b9e61d8a3d7da57c23
ivancraze <ivancraze@yandex.ru>
2020-05-27 23:03:37 +0400

    4 урок сделал

--- udemy.js
+++ udemy.js
@@ -1,26 +1,94 @@
 "use strict";
 
-const str = "test";
-const arr = [1, 2, 4];
+// let a = 5,
+//     b = a;
 
-console.log(str.toUpperCase());
-console.log(str);
+// b = b + 5;
 
-const fruit = "Some fruit";
+// console.log(b);
+// console.log(a);
 
-console.log(fruit.indexOf("fruit"));
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
-const logg = "Hello world";
+// const copy = obj; //ссылка
 
-console.log(logg.slice(6, 11));
+// copy.a = 10;
 
-console.log(logg.substring(6, 11));
+// console.log(copy);
+// console.log(obj);
 
-console.log(logg.substr(6, 2));
+function copy(mainObj) {
+    let objCopy = {};
 
-const num = 12.2;
-console.log(Math.round(num));
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
 
-const test = "12.2px";
-// console.log(parseInt(test));
-console.log(parseFloat(test));
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+const  add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'salkdjfh';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
